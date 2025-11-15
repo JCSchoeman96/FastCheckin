6a73378501d60d8dcb42def0d62a2e731b89a660
JCSchoeman96 <jcschoeman123@gmail.com>
2025-11-15 12:09:17 +0200

    Make optimization indexes reversible

--- priv/repo/migrations/20250206000000_create_indexes_optimization.exs
+++ priv/repo/migrations/20250206000000_create_indexes_optimization.exs
@@ -1,19 +1,62 @@
 defmodule PetalBlueprint.Repo.Migrations.CreateIndexesOptimization do
   use Ecto.Migration
 
-  def change do
-    # EXPLAIN ANALYZE SELECT * FROM attendees WHERE event_id = 42 ORDER BY checked_in_at DESC LIMIT 10;
-    # Index Scan using idx_attendees_event_checked on attendees  (actual time=0.07..0.38 rows=10 loops=1)
-    create_if_not_exists index(:attendees, [:event_id, :checked_in_at],
-                           name: :idx_attendees_event_checked)
+  @managed_comment "created_by_migration_20250206000000"
 
-    # EXPLAIN ANALYZE SELECT * FROM attendees WHERE event_id = 42 AND ticket_code = 'ABC123' LIMIT 1 FOR UPDATE;
-    # Index Scan using idx_attendees_event_code on attendees  (actual time=0.05..0.08 rows=1 loops=1)
-    create_if_not_exists index(:attendees, [:event_id, :ticket_code],
-                           name: :idx_attendees_event_code)
+  def up do
+    ensure_index(:attendees, [:event_id, :checked_in_at], :idx_attendees_event_checked)
+    ensure_index(:attendees, [:event_id, :ticket_code], :idx_attendees_event_code)
+    ensure_index(:check_ins, [:entrance_name], :idx_check_ins_entrance)
+  end
 
-    # EXPLAIN ANALYZE SELECT entrance_name, count(*) FROM check_ins WHERE entrance_name = 'North Gate';
-    # Index Only Scan using idx_check_ins_entrance on check_ins  (actual time=0.04..0.06 rows=1 loops=1)
-    create_if_not_exists index(:check_ins, [:entrance_name], name: :idx_check_ins_entrance)
+  def down do
+    drop_managed_index(:idx_attendees_event_checked)
+    drop_managed_index(:idx_attendees_event_code)
+    drop_managed_index(:idx_check_ins_entrance)
+  end
+
+  defp ensure_index(table, columns, name) do
+    table_sql = Atom.to_string(table)
+    index_sql = Atom.to_string(name)
+    columns_sql =
+      columns
+      |> Enum.map(&"\"#{Atom.to_string(&1)}\"")
+      |> Enum.join(", ")
+
+    execute("""
+    DO $$
+    BEGIN
+      IF NOT EXISTS (
+        SELECT 1 FROM pg_class WHERE relname = '#{index_sql}' AND relkind = 'i'
+      ) THEN
+        EXECUTE 'CREATE INDEX #{index_sql} ON #{table_sql} (#{columns_sql})';
+        EXECUTE 'COMMENT ON INDEX #{index_sql} IS ''#{@managed_comment}''';
+      END IF;
+    END;
+    $$;
+    """)
+  end
+
+  defp drop_managed_index(name) do
+    index_sql = Atom.to_string(name)
+
+    execute("""
+    DO $$
+    DECLARE
+      idx_oid oid;
+      idx_comment text;
+    BEGIN
+      SELECT c.oid, d.description
+      INTO idx_oid, idx_comment
+      FROM pg_class c
+      LEFT JOIN pg_description d ON d.objoid = c.oid AND d.classoid = 'pg_class'::regclass
+      WHERE c.relname = '#{index_sql}' AND c.relkind = 'i';
+
+      IF idx_oid IS NOT NULL AND idx_comment = '#{@managed_comment}' THEN
+        EXECUTE 'DROP INDEX IF EXISTS #{index_sql}';
+      END IF;
+    END;
+    $$;
+    """)
   end
 end
