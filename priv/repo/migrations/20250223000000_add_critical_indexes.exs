4f707bce1d14e0b699f54fe6cd7cdfd1de1287f7
JCSchoeman96 <jcschoeman123@gmail.com>
2025-11-15 19:46:25 +0200

    Add critical database indexes

---
+++ priv/repo/migrations/20250223000000_add_critical_indexes.exs
@@ -0,0 +1,23 @@
+defmodule PetalBlueprint.Repo.Migrations.AddCriticalIndexes do
+  use Ecto.Migration
+
+  def change do
+    create_if_not_exists(unique_index(:attendees, [:event_id, :ticket_code], name: :idx_attendees_event_code))
+
+    create_if_not_exists(index(:attendees, [:event_id, :checked_in_at], name: :idx_attendees_event_checked))
+
+    create_if_not_exists(index(:attendees, [:event_id, :is_currently_inside], name: :idx_attendees_event_inside))
+
+    create_if_not_exists(index(:check_ins, [:event_id, :entrance_name, :checked_in_at],
+      name: :idx_check_ins_event_entrance_checked_in
+    ))
+
+    create_if_not_exists(index(:check_ins, [:event_id, :status, :checked_in_at],
+      name: :idx_check_ins_event_status_checked_in
+    ))
+
+    create_if_not_exists(index(:check_in_sessions, [:attendee_id, :exit_time],
+      name: :idx_check_in_sessions_attendee_exit_time
+    ))
+  end
+end
