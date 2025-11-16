a45c0b4e9b38f5f8b6914664c4e1c80156b66d73
JCSchoeman96 <jcschoeman123@gmail.com>
2025-11-16 07:20:00 +0200

    Handle optional formatter deps

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,16 @@
+deps_with_modules = [
+  phoenix: Phoenix.Router,
+  ecto: Ecto.Schema,
+  ecto_sql: Ecto.Migration
+]
+
+imported_deps =
+  for {dep, module} <- deps_with_modules,
+      Code.ensure_loaded?(module),
+      do: dep
+
 [
-  import_deps: [:ecto, :ecto_sql, :phoenix],
+  import_deps: imported_deps,
   subdirectories: ["priv/*/migrations"],
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
--- priv/repo/migrations/.formatter.exs
+++ priv/repo/migrations/.formatter.exs
@@ -1,4 +1,11 @@
+deps_with_modules = [ecto_sql: Ecto.Migration]
+
+imported_deps =
+  for {dep, module} <- deps_with_modules,
+      Code.ensure_loaded?(module),
+      do: dep
+
 [
-  import_deps: [:ecto_sql],
+  import_deps: imported_deps,
   inputs: ["*.exs"]
 ]
