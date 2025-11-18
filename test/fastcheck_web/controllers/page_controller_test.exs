2111f01691a4d949411fe9037ea56a0fc3427995
JCSchoeman96 <jcschoeman123@gmail.com>
2025-11-18 09:37:55 +0200

    Remove unused page controller

--- test/fastcheck_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule FastCheckWeb.PageControllerTest do
-  use FastCheckWeb.ConnCase
-
-  test "GET /phoenix", %{conn: conn} do
-    conn = get(conn, ~p"/phoenix")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
