b278d5fbfd529942d3b54ab16d512dc631d77793
JCSchoeman96 <jcschoeman123@gmail.com>
2025-11-18 13:56:43 +0200

    Add symmetric encryption helper

--- lib/fastcheck/crypto.ex
+++ lib/fastcheck/crypto.ex
@@ -1,102 +1,67 @@
 defmodule FastCheck.Crypto do
   @moduledoc """
-  Symmetric encryption helper used to protect external credentials at rest.
+  Symmetric encryption helpers backed by a master key configured for the app.
   """
-
-  require Logger
 
   alias Plug.Crypto.{KeyGenerator, MessageEncryptor}
 
-  @encryption_salt "fastcheck:tickera:enc"
-  @signing_salt "fastcheck:tickera:sig"
-
   @doc """
-  Encrypts the provided binary using the application secret key base.
-
-  Returns `{:ok, ciphertext}` on success.
+  Encrypts the given `plaintext` and returns a Base64 encoded ciphertext safe for storage.
   """
   @spec encrypt(binary()) :: {:ok, binary()} | {:error, term()}
-  def encrypt(value) when is_binary(value) do
-    with {:ok, secret_key_base} <- secret_key_base(),
-         {:ok, ciphertext} <- do_encrypt(secret_key_base, value) do
+  def encrypt(plaintext) when is_binary(plaintext) do
+    try do
+      ciphertext =
+        plaintext
+        |> MessageEncryptor.encrypt(encryption_secret(), signing_secret())
+        |> Base.encode64()
+
       {:ok, ciphertext}
-    else
-      {:error, reason} ->
-        Logger.error("Credential encryption failed: #{inspect(reason)}")
-        {:error, :encryption_failed}
+    rescue
+      error -> {:error, error}
     end
-  rescue
-    exception ->
-      Logger.error("Credential encryption exception: #{Exception.message(exception)}")
-      {:error, :encryption_failed}
   end
 
-  def encrypt(_), do: {:error, :invalid_data}
+  def encrypt(_), do: {:error, :invalid_plaintext}
 
   @doc """
-  Decrypts a ciphertext produced by `encrypt/1`.
+  Decrypts the given Base64 encoded ciphertext and returns the plaintext on success.
   """
-  @spec decrypt(binary()) :: {:ok, binary()} | {:error, :decryption_failed}
+  @spec decrypt(binary()) :: {:ok, binary()} | {:error, term()}
   def decrypt(ciphertext) when is_binary(ciphertext) do
-    with {:ok, secret_key_base} <- secret_key_base(),
-         {:ok, plaintext} <- do_decrypt(secret_key_base, ciphertext) do
+    with {:ok, encoded} <- Base.decode64(ciphertext),
+         {:ok, plaintext} <- do_decrypt(encoded) do
       {:ok, plaintext}
     else
-      {:error, _reason} -> {:error, :decryption_failed}
-    end
-  rescue
-    _exception -> {:error, :decryption_failed}
-  end
-
-  def decrypt(_), do: {:error, :decryption_failed}
-
-  defp do_encrypt(secret_key_base, value) do
-    {secret, sign_secret} = derive_secrets(secret_key_base)
-    {:ok, MessageEncryptor.encrypt(value, secret, sign_secret)}
-  rescue
-    exception -> {:error, exception}
-  end
-
-  defp do_decrypt(secret_key_base, ciphertext) do
-    {secret, sign_secret} = derive_secrets(secret_key_base)
-    {:ok, MessageEncryptor.decrypt(ciphertext, secret, sign_secret)}
-  rescue
-    _exception -> {:error, :invalid_ciphertext}
-  end
-
-  defp derive_secrets(secret_key_base) do
-    secret = KeyGenerator.generate(secret_key_base, @encryption_salt, length: 32, iterations: 1000)
-    sign_secret = KeyGenerator.generate(secret_key_base, @signing_salt, length: 32, iterations: 1000)
-    {secret, sign_secret}
-  end
-
-  defp secret_key_base do
-    case fetch_endpoint_secret() do
-      nil -> {:error, :missing_secret_key_base}
-      secret -> {:ok, secret}
+      :error -> {:error, :invalid_base64}
+      {:error, reason} -> {:error, reason}
     end
   end
 
-  defp fetch_endpoint_secret do
-    case fastcheck_endpoint_secret() do
-      nil -> runtime_endpoint_secret()
-      secret -> secret
-    end
-  end
+  def decrypt(_), do: {:error, :invalid_ciphertext}
 
-  defp fastcheck_endpoint_secret do
+  defp do_decrypt(encoded) do
     try do
-      FastCheckWeb.Endpoint.config(:secret_key_base)
+      plaintext = MessageEncryptor.decrypt(encryption_secret(), signing_secret(), encoded)
+      {:ok, plaintext}
     rescue
-      _ -> nil
+      _ -> {:error, :invalid_ciphertext}
     end
   end
 
-  defp runtime_endpoint_secret do
-    Application.get_env(:fastcheck, FastCheckWeb.Endpoint)
-    |> case do
-      nil -> nil
-      config -> Keyword.get(config, :secret_key_base)
-    end
+  defp encryption_secret do
+    derive_secret("fastcheck:encryption")
+  end
+
+  defp signing_secret do
+    derive_secret("fastcheck:signing")
+  end
+
+  defp derive_secret(salt) do
+    KeyGenerator.generate(master_key(), salt, iterations: 1000, length: 32)
+  end
+
+  defp master_key do
+    Application.fetch_env!(:fastcheck, :encryption_key)
   end
 end
