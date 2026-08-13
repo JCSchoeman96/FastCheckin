3776ced5092d4d7e77794eb016e85ef4f2cafc59
JCSchoeman96 <jcschoeman123@gmail.com>
2026-08-13 13:08:20 +0200

    Fix stochastic ticket code input assertion

--- test/fastcheck/tickets/code_generator_test.exs
+++ test/fastcheck/tickets/code_generator_test.exs
@@ -15,18 +15,10 @@ defmodule FastCheck.Tickets.CodeGeneratorTest do
     assert length(Enum.uniq(codes)) == 50
   end
 
-  test "generate/0 is not derived from caller inputs" do
-    order_id = 12_345
-    event_id = 99
+  test "generate/0 accepts no caller inputs" do
+    public_functions = CodeGenerator.__info__(:functions)
 
-    code =
-      order_id
-      |> Integer.to_string()
-      |> then(fn _ ->
-        event_id |> Integer.to_string() |> then(fn _ -> CodeGenerator.generate() end)
-      end)
-
-    refute code =~ Integer.to_string(order_id)
-    refute code =~ Integer.to_string(event_id)
+    assert {:generate, 0} in public_functions
+    refute {:generate, 1} in public_functions
   end
 end
