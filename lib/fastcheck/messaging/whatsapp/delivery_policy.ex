1d72a2797ff282ad371b893103d4160413900d7d
JCSchoeman96 <jcschoeman123@gmail.com>
2026-09-17 21:47:35 +0200

    refactor: share WhatsApp delivery policy selection

--- lib/fastcheck/messaging/whatsapp/delivery_policy.ex
+++ lib/fastcheck/messaging/whatsapp/delivery_policy.ex
@@ -17,49 +17,15 @@ defmodule FastCheck.Messaging.WhatsApp.DeliveryPolicy do
 
   @spec select_ticket_delivery(map() | struct(), keyword()) :: decision()
   def select_ticket_delivery(conversation, opts \\ []) do
-    now = Keyword.get(opts, :now, DateTime.utc_now())
-    fetch_template = Keyword.get(opts, :fetch_template, &TemplateCatalog.fetch/1)
-    last_message_at = value(conversation, :last_message_at)
-    within_window? = DeliveryWindow.inside?(last_message_at, now)
-
-    if within_window? do
-      %{
-        mode: :session_message,
-        within_whatsapp_window: true,
-        template_key: nil,
-        template: nil,
-        fallback_channel: nil,
-        failure_reason: nil
-      }
-    else
-      template_key = ticket_ready_template_key(value(conversation, :preferred_language))
-
-      case fetch_template.(template_key) do
-        {:ok, template} ->
-          %{
-            mode: :template_message,
-            within_whatsapp_window: false,
-            template_key: template_key,
-            template: template,
-            fallback_channel: nil,
-            failure_reason: nil
-          }
-
-        :error ->
-          %{
-            mode: :fallback_required,
-            within_whatsapp_window: false,
-            template_key: template_key,
-            template: nil,
-            fallback_channel: "manual_review",
-            failure_reason: "whatsapp_template_unavailable"
-          }
-      end
-    end
+    select_delivery(conversation, opts, &ticket_ready_template_key/1)
   end
 
   @spec select_payment_link_delivery(map() | struct(), keyword()) :: decision()
   def select_payment_link_delivery(conversation, opts \\ []) do
+    select_delivery(conversation, opts, &payment_link_template_key/1)
+  end
+
+  defp select_delivery(conversation, opts, template_key_selector) do
     now = Keyword.get(opts, :now, DateTime.utc_now())
     fetch_template = Keyword.get(opts, :fetch_template, &TemplateCatalog.fetch/1)
     last_message_at = value(conversation, :last_message_at)
@@ -75,7 +41,7 @@ defmodule FastCheck.Messaging.WhatsApp.DeliveryPolicy do
         failure_reason: nil
       }
     else
-      template_key = payment_link_template_key(value(conversation, :preferred_language))
+      template_key = template_key_selector.(value(conversation, :preferred_language))
 
       case fetch_template.(template_key) do
         {:ok, template} ->
